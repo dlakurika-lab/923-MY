539c818c0cc9623687dba94d4e1994d42acc31cc
dlakurika <dlakurika@example.com>
2026-05-13 18:48:27 +0600

    Завершение 1пр и начало работы 2 пр z1

---
+++ Практическая работа № 1/z2/php — копия/public/index.php
@@ -0,0 +1,13 @@
+php/public/index.php 
+<!doctype html> 
+<html lang="ru"> 
+  <head> 
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+    <title>Пример</title> 
+  </head> 
+  <body> 
+    <?php 
+      echo "<h1>Добро пожаловать!</h1>"; 
+    ?> 
+  </body> 
+</html>
