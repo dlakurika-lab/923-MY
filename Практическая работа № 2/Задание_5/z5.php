2a6af77d6ab898db6f2957b024afba9ddeb571a1
dlakurika <dlakurika@example.com>
2026-05-14 11:01:41 +0600

    2ПР z4

---
+++ Практическая работа № 2/Задание_5/z5.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Программирование на языке PHP</title>
+</head>
+<body>
+	
+	<h1>Основы программирования</h1>
+	<h2>Переменные</h2>
+	<hr>
+
+	
+	<?php
+		$usd = 1000;
+        $rub = 75.87;
+        $cny = 0.09;
+	?>
+	
+
+</body>
+</html>
